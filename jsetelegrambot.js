7cfb4d521c9c70f2ad1d68826afb9f55a9e77bbd
James Bachini <james@jsecoin.com>
2019-11-19 10:40:06 +0000

    naughtyMessage notification and reduce timeframe to 90 seconds

--- jsetelegrambot.js
+++ jsetelegrambot.js
@@ -1,7 +1,7 @@
 const Telegraf = require('telegraf')
 const request = require('request');
 
-const version = `v0.1.0`;
+const version = `v0.1.1`;
 
 const credentials = require('./credentials.json'); // {"token":"123"} https://core.telegram.org/bots
 
@@ -19,40 +19,51 @@ bot.on('message', async (ctx) => {
 	// delete bad words
 	if (ctx.update.message.text && msg.match(/t.me\/joinchat/i)) {
 		ctx.telegram.deleteMessage(chatID, msgID);
-		console.log(`DELETED MSG: ${chatID} - ${msgID}`);
+		console.log(`DELETED JOIN LINK: ${chatID} - ${msgID}`);
+		const naughtyMessage = `Links to other channels are not allowed to prevent spam.`;
+		const naughtyMsgTicket = await ctx.reply(naughtyMessage);
+		const naughtyMsgID = naughtyMsgTicket.message_id;
+		setTimeout(() => {
+			ctx.telegram.deleteMessage(chatID, naughtyMsgID);
+		}, 10000);
 	}
 	// message new users about captcha
 	if (ctx.update.message.new_chat_members) {
 		ctx.update.message.new_chat_members.forEach(async (newUser) => {
 			console.log(`New User Joined ${newUser.username} ID: ${newUser.id}`);
-			const introMessage = `Hello @${newUser.username}, please complete the captcha at https://jsecoin.com/telegram-captcha.php?u=${newUser.id} within the next 3 minutes and before posting a message. This is to prevent spam on the Telegram channel.`;
+			const introMessage = `Hello @${newUser.username}, please complete the captcha at https://jsecoin.com/telegram-captcha.php?u=${newUser.id} within the next 90 seconds and before posting a message. This is to prevent spam on the Telegram channel.`;
 			const introMsgTicket = await ctx.reply(introMessage);
 			//console.log(introMsgTicket);
 			const introMsgID = introMsgTicket.message_id;
 			setTimeout(() => {
 				ctx.telegram.deleteMessage(chatID, introMsgID);
-			}, 180000);
-
+			}, 60000);
 			newUsers.push(newUser.id);
 			setTimeout(() => {
 				checkCaptcha(newUser.id);
 			}, 10000);
 			setTimeout(() => {
 				kickUser(ctx,chatID,newUser.id);
-			}, 180000);
+			}, 90000);
 		});
 	}
 	// block new users
 	if (ctx.update.message.text && newUsers.indexOf(ctx.update.message.from.id) > -1) {
 		ctx.telegram.deleteMessage(chatID, msgID);
-		console.log(`DELETED MSG: ${chatID} - ${msgID}`);
+		console.log(`DELETED NEW USER MSG: ${chatID} - ${msgID}`);
+		const naughtyMessage = `New users are not allowed to post messages until the captcha has been authenticated.`;
+		const naughtyMsgTicket = await ctx.reply(naughtyMessage);
+		const naughtyMsgID = naughtyMsgTicket.message_id;
+		setTimeout(() => {
+			ctx.telegram.deleteMessage(chatID, naughtyMsgID);
+		}, 10000);
 	}
 });
 
 /* check if the user has completed the captcha and remove from array if so */
 const checkCaptcha = ((userID) => {
 	if (newUsers.indexOf(userID) === -1) return false;
-	console.log(`Checking Captcha: ${userID}`);
+	//console.log(`Checking Captcha: ${userID}`);
 	request(`https://jsecoin.com/telegram-captcha.php?c=${userID}`, function (error, response, body) {
 		if (body) {
 			if (body === '1') {
@@ -70,7 +81,7 @@ const checkCaptcha = ((userID) => {
 	});
 });
 
-/* kick user if they haven't done captcha in 3 mins */
+/* kick user if they haven't done captcha */
 const kickUser = ((ctx,chatID,userID) => {
 	if (newUsers.indexOf(userID) > -1) {
 		console.log(`Kicking User: ${userID} - ${chatID}`);
